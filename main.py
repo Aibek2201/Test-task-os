7ab3488d4402d93195534b0295a1c403d74b75dd
Aibek2201 <aibekalmabekuly@gmail.com>
2024-07-25 14:44:45 +0500

    added exception permission denied

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ def categorize_files_by_type(folder_path):
     res = {}
 
     def scan_dir(dir_name):
-        with os.scandir(dir_name) as it:
-            for entry in it:
-
-                if entry.is_file():
-                    ext = os.path.splitext(entry.name)[1]
-                    if ext not in res:
-                        res[ext] = []
-                    res[ext].append(entry.path)
-                elif entry.is_dir():
-                    scan_dir(entry.path)  # Recursion
+        try:
+            with os.scandir(dir_name) as it:
+                for entry in it:
+                    if entry.is_file():
+                        ext = os.path.splitext(entry.name)[1]
+                        if ext not in res:
+                            res[ext] = []
+                        res[ext].append(entry.path)
+                    elif entry.is_dir():
+                        scan_dir(entry.path)  # Recursion
+        except PermissionError:
+            print(f"Permission denied: {dir_name}")
 
     scan_dir(folder_path)
     return res
 
 
-result = categorize_files_by_type(r"C:\Users\77475\Desktop\TestTask\paths")
+result = categorize_files_by_type(r"C:\Users")
 print(result)
