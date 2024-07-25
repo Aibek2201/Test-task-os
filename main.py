798aae5bb26bf1f84d6cca006d7b05428375dd9d
Aibek2201 <aibekalmabekuly@gmail.com>
2024-07-25 12:35:38 +0500

    remade function via recursion

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import os
 
 
 def categorize_files_by_type(folder_path):
+
     res = {}
 
+    def scan_dir(dir_name):
+        with os.scandir(dir_name) as it:
+            for entry in it:
 
-    for root, dirs, files in os.walk(folder_path):
-        print(root, dirs, files)
-        for file in files:
-            file_extension = os.path.splitext(file)[1]
-            full_path = os.path.join(root, file)
+                if entry.is_file():
+                    ext = os.path.splitext(entry.name)[1]
+                    if ext not in res:
+                        res[ext] = []
+                    res[ext].append(entry.path)
+                elif entry.is_dir():
+                    scan_dir(entry.path)
 
-            if file_extension in res:
-                res[file_extension].append(full_path)
-            else:
-                res[file_extension] = [full_path]
-
+    scan_dir(folder_path)
     return res
 
 
-
-result = categorize_files_by_type(r"C:\Users\77475\Desktop\TestTask")
+result = categorize_files_by_type(r"C:\Users\77475\Desktop\TestTask\paths")
 print(result)
